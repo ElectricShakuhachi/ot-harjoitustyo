bea4044e61cd34fbfa8fef57ef8cedc0b0080467
ElectricShakuhachi <haavistoaaro@gmail.com>
2021-12-14 23:17:36 +0200

    configure coverage

--- src/tests/ui_test.py
+++ src/tests/ui_test.py
@@ -1,10 +1,10 @@
 import unittest
 from tkinter import Tk, Canvas
-from ui.view import UI 
+from ui.view import View
 
 class TestUI(unittest.TestCase):
     def setUp(self):
-        self.ui = UI(Tk())
+        self.ui = View(Tk())
 
     def test_starting_ui_creates_canvas(self):
         self.assertEqual(type(self.ui.sheet), type(Canvas()))
